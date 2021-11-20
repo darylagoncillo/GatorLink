d18a14f95a0e17a7ba3e369cc80d881ebaa371f3
jcastan6 <josecastanon4@gmail.com>
2021-11-19 18:49:40 -0800

    first commit

---
+++ src/client/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import "./app.css";
+import Home from "./Components/Home";
+
+export default class Routing extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Route
+          exact
+          path="/"
+          render={(props) => <Home {...props} component={Home} />}
+        />
+
+        {/* <Route component={Home} path="/home" /> */}
+      </BrowserRouter>
+    );
+  }
+}
