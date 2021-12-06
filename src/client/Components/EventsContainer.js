e81cc1b9aeb37aa154694387e51ef1281793c275
jcastan6 <josecastanon4@gmail.com>
2021-12-05 20:57:37 -0800

    added event cards, event data

--- src/client/Components/EventsContainer.js
+++ src/client/Components/EventsContainer.js
@@ -7,14 +7,36 @@ import {
   Nav,
   Form,
   FormControl,
+  Card,
   NavDropdown,
 } from "react-bootstrap";
 import React, { Component } from "react";
+import data from "../../../data.json";
+import "./components.css";
 
 export default class EventsContainer extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+
+    this.state = {
+      events: data.Events,
+    };
+
+    this.renderEvents = this.renderEvents.bind(this);
+  }
+
+  renderEvents() {
+    return this.state.events.map((item) => (
+      <Col md="4">
+        <Card className="event-card">
+          <Card.Img variant="top" src={item.image_url} />
+          <Card.Body>
+            <Card.Title>{item.event_name}</Card.Title>
+            <Card.Text>{item.date}</Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+    ));
   }
 
   render() {
@@ -43,6 +65,8 @@ export default class EventsContainer extends Component {
             </Navbar.Collapse>
           </Container>
         </Navbar>
+        <br />
+        <Row>{this.renderEvents()}</Row>
       </div>
     );
   }
