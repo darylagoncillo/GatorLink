2c06a63f491255a4aa53403c078b816c39c7f5d8
jcastan6 <josecastanon4@gmail.com>
2021-12-12 22:30:04 -0800

    updated search bar

--- src/client/Components/Navigation.js
+++ src/client/Components/Navigation.js
@@ -11,18 +11,56 @@ import {
 import { withRouter } from "react-router";
 import "./components.css";
 import Select from "react-select";
+import data from "../../../event-data.json";
+import data2 from "../../../group-data.json";
 
+const events = [];
+data.Events.map((item) => {
+  events.push({ label: item.event_name, value: item.event_name });
+});
+const groups = [];
+data2.Groups.map((item) => {
+  groups.push({ label: item.group_name, value: item.group_name });
+});
 const options = [
-  { value: "chocolate", label: "Chocolate" },
-  { value: "strawberry", label: "Strawberry" },
-  { value: "vanilla", label: "Vanilla" },
+  {
+    label: "Events",
+    options: events,
+  },
+  {
+    label: "Groups",
+    options: groups,
+  },
 ];
 
 class Navigation extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      searchValue: "",
+      menuOpen: false,
+    };
+    this.search = this.search.bind(this);
   }
+
+  search() {
+    this.props.history.push("/Search");
+  }
+
+  onChange = (selectedOption) => {
+    this.setState({ searchValue: selectedOption });
+    console.log("Option selected:", selectedOption);
+  };
+
+  change = (event) => {
+    console.log(event);
+
+    if (event === "") {
+      this.setState({ searchValue: event, menuOpen: false });
+    } else {
+      this.setState({ searchValue: event, menuOpen: true });
+    }
+  };
 
   render() {
     const { location } = this.props;
@@ -43,8 +81,17 @@ class Navigation extends Component {
                 <Nav.Link href="/Groups">Groups</Nav.Link>
               </Nav>
               <Nav className="ml-auto">
-                <div style={{ width: "400px" }}>
-                  <Select options={options} />
+                <div className="search-bar">
+                  <Form onSubmit={this.search}>
+                    <Select
+                      onInputChange={(event) => this.change(event)}
+                      value={this.state.searchValue}
+                      onChange={this.onChange}
+                      options={options}
+                      menuIsOpen={this.state.menuOpen}
+                      placeholder="Search"
+                    />
+                  </Form>
                 </div>
               </Nav>
               <Nav className="ml-auto">
