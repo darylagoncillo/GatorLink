373a009f2773cc0de9a9aad71804d12f16e4c3c0
Daryl <darylgio1@gmail.com>
2021-12-13 19:18:15 -0800

    edit profile page progress

--- src/client/Templates/ProfileEditor.js
+++ src/client/Templates/ProfileEditor.js
@@ -16,20 +16,14 @@ import {
   InputGroup
 } from "react-bootstrap";
 import React, { Component } from "react";
-import "../Components/Cards.css";
+import "../Components/ProfileEditor.css";
 import Modal from "react-modal";
 import {
-  BsArrowLeft,
-  BsThreeDotsVertical,
-  BsListUl,
   BsFillPersonBadgeFill,
   BsFillShieldFill,
 } from "react-icons/bs";
-import { ImProfile } from "react-icons/im";
-import { GoDeviceCamera } from "react-icons/go";
 import { AiFillBell } from "react-icons/ai";
 import Navigation from "../Components/Navigation";
-import EPSidebar from "../Components/EPSidebar";
 import profilePicture from "../../images/profile-img.jpg";
 
 export default class ProfileEditor extends Component {
@@ -82,55 +76,209 @@ export default class ProfileEditor extends Component {
                           <Navbar.Brand href="/EditProfile">
                             Edit {this.state.selectedCategory} Profile
                           </Navbar.Brand>
+                          <Col xs={6} md={4}>
+                            <Image src={profilePicture} fluid roundedCircle />
+                          </Col>
+                          <Form>
+                            <Row className="align-items-center">
+                              <Col xs="auto">
+                                <Form.Label htmlFor="inlineFormInput" visuallyHidden>
+                                  First Name
+                                </Form.Label>
+                                <Form.Control
+                                  className="mb-2"
+                                  id="inlineFormInput"
+                                  placeholder="Jane Doe"
+                                />
+                              </Col>
+                              <Col xs="auto">
+                                <Form.Label htmlFor="inlineFormInput" visuallyHidden>
+                                  Last Name
+                                </Form.Label>
+                                <Form.Control
+                                  className="mb-2"
+                                  id="inlineFormInput"
+                                  placeholder="Jane Doe"
+                                />
+                              </Col>
+                              <Col xs="auto">
+                                <Form.Label htmlFor="inlineFormInputGroup" visuallyHidden>
+                                  Username
+                                </Form.Label>
+                                <InputGroup className="mb-2">
+                                  <InputGroup.Text>@</InputGroup.Text>
+                                  <FormControl id="inlineFormInputGroup" placeholder="Username" />
+                                </InputGroup>
+                              </Col>
+                            </Row>
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                              <Form.Label>About Me</Form.Label>
+                              <Form.Control as="textarea" rows={3} />
+                            </Form.Group>
+                            <button type="button" class="btn btn-primary float-right">Save</button>
+                          </Form>
                         </Tab.Pane>
-                        <Tab.Pane eventKey="second">Notifications</Tab.Pane>
-                        <Tab.Pane eventKey="third">Privacy</Tab.Pane>
+
+                        <Tab.Pane eventKey="second">
+                          <Navbar.Brand>
+                            Notifications
+                          </Navbar.Brand>
+                          <Col>
+                            <h6>Comments</h6>
+                            <Form>
+                              <Form.Switch
+                                type="switch"
+                                id="cpush"
+                                label="Push"
+                              />
+                              <Form.Switch
+                                type="switch"
+                                id="cemail"
+                                label="Email"
+                              />
+                              <Form.Switch
+                                type="switch"
+                                id="csms"
+                                label="SMS"
+                              />
+                            </Form>
+                            <h6>Reminders</h6>
+                            <Form>
+                              <Form.Switch
+                                type="switch"
+                                id="rpush"
+                                label="Push"
+                              />
+                              <Form.Switch
+                                type="switch"
+                                id="remail"
+                                label="Email"
+                              />
+                              <Form.Switch
+                                type="switch"
+                                id="rsms"
+                                label="SMS"
+                              />
+                            </Form>
+                            <h6>Update From Friends</h6>
+                            <Form>
+                              <Form.Switch
+                                type="switch"
+                                id="upush"
+                                label="Push"
+                              />
+                              <Form.Switch
+                                type="switch"
+                                id="uemail"
+                                label="Email"
+                              />
+                              <Form.Switch
+                                type="switch"
+                                id="usms"
+                                label="SMS"
+                              />
+                            </Form>
+                            <h6>Events</h6>
+                            <Form>
+                              <Form.Switch
+                                type="switch"
+                                id="epush"
+                                label="Push"
+                              />
+                              <Form.Switch
+                                type="switch"
+                                id="eemail"
+                                label="Email"
+                              />
+                              <Form.Switch
+                                type="switch"
+                                id="esms"
+                                label="SMS"
+                              />
+                            </Form>
+                            <h6>Marketplace</h6>
+                            <Form>
+                              <Form.Switch
+                                type="switch"
+                                id="mpush"
+                                label="Push"
+                              />
+                              <Form.Switch
+                                type="switch"
+                                id="memail"
+                                label="Email"
+                              />
+                              <Form.Switch
+                                type="switch"
+                                id="msms"
+                                label="SMS"
+                              />
+                            </Form>
+                            <br></br>
+                          </Col>
+                          
+                        </Tab.Pane>
+
+                        <Tab.Pane eventKey="third">
+                          <Navbar.Brand>
+                            Privacy
+                          </Navbar.Brand>
+                          <Col>
+                            <h6>1. Choose who you want to see your posts</h6>
+                            <Form>
+                              <Form.Switch
+                                type="switch"
+                                id="public"
+                                label="Public"
+                              />
+                              <Form.Switch
+                                type="switch"
+                                id="friends"
+                                label="Friends"
+                              />
+                              <Form.Switch
+                                type="switch"
+                                id="private"
+                                label="Only Me"
+                              />
+                            </Form>
+                            <h6>2. Who can send you friend requests?</h6>
+                            <Form>
+                              <Form.Switch
+                                type="switch"
+                                id="friendPublic"
+                                label="Everyone"
+                              />
+                              <Form.Switch
+                                type="switch"
+                                id="friendFriends"
+                                label="Mutual Friends"
+                              />
+                            </Form>
+                            <h6>3. Who can send you messages?</h6>
+                            <Form>
+                              <Form.Switch
+                                type="switch"
+                                id="messagePublic"
+                                label="Public"
+                              />
+                              <Form.Switch
+                                type="switch"
+                                id="messageFriends"
+                                label="Friends"
+                              />
+                              <Form.Switch
+                                type="switch"
+                                id="messageFoF"
+                                label="Mutual Friends"
+                              />
+                            </Form>
+                            <br></br>
+                          </Col>
+                        </Tab.Pane>
                       </Tab.Content>
                     </Container>
                   </Navbar>
-                  <Col xs={6} md={4}>
-                    <Image src={profilePicture} fluid roundedCircle />
-                  </Col>
-                  <Form>
-                    <Row className="align-items-center">
-                      <Col xs="auto">
-                        <Form.Label htmlFor="inlineFormInput" visuallyHidden>
-                          First Name
-                        </Form.Label>
-                        <Form.Control
-                          className="mb-2"
-                          id="inlineFormInput"
-                          placeholder="Jane Doe"
-                        />
-                      </Col>
-                      <Col xs="auto">
-                        <Form.Label htmlFor="inlineFormInput" visuallyHidden>
-                          Last Name
-                        </Form.Label>
-                        <Form.Control
-                          className="mb-2"
-                          id="inlineFormInput"
-                          placeholder="Jane Doe"
-                        />
-                      </Col>
-                      <Col xs="auto">
-                        <Form.Label htmlFor="inlineFormInputGroup" visuallyHidden>
-                          Username
-                        </Form.Label>
-                        <InputGroup className="mb-2">
-                          <InputGroup.Text>@</InputGroup.Text>
-                          <FormControl id="inlineFormInputGroup" placeholder="Username" />
-                        </InputGroup>
-                      </Col>
-                    </Row>
-                    <Col>
-                      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                        <Form.Label>About Me</Form.Label>
-                        <Form.Control as="textarea" rows={3} />
-                      </Form.Group>
-                      <button type="button" class="btn btn-primary float-right">Save</button>
-                    </Col>
-                  </Form>
                 </div>
               </Col>
             </Row>
